a67d2713b2bb1a421f977f28866d0774a4bd4c17
Chamster <nikkovkel258@gmail.com>
2022-10-03 23:58:23 +0300

    UIDT (ТРПИ)

--- Course 2/Term 1/User Interface Development Technologies/01. Javascript Basics/main.js
+++ Course 2/Term 1/User Interface Development Technologies/01. Javascript Basics/main.js
@@ -1,13 +1,4 @@
-// function hello(name) {
-//     let phrase = `Hello, ${name}!`;
-    
-//     say(phrase);
-// }
-
-// function say(phrase) {
-//     alert(`** ${phrase} **`)
-// }
-
+//---------------------1---------------------
 // function pow(x, n){
 //     let result = 1;
 //     for (var i = 0; i < n; i++){
@@ -22,9 +13,11 @@
 // if (n < 0){
 //     alert(`Cтепень ${n} не поддерживается, введите целую степень, большую 0`);
 // }
+//else{
+//  alert(pow(x, n));  
+//}
 
-//---------------------------------------------
-
+//---------------------2---------------------
 let userName = 'Chamster';
 let city = 'Minsk';
 const BIRTHDAY = 2004;
@@ -35,30 +28,53 @@ let number = 532;
 let perimeter = '120mm';
 let message = 'Введенные данные неверны';
 
-// //---------------------------------------------
+//---------------------3---------------------
+// let a = 5;
+// let name = "Name";
+// let i = 0;
+// let double = 0.23;
+// let result = 1/0;
+// let answer = true;
+// let no = null;
+
+//---------------------4---------------------
 function getArea(firstRectangelSide, secondRectangelSide){
     return firstRectangelSide * secondRectangelSide;
 }
 
-// //---------------------------------------------
+//---------------------5---------------------
 
 function howManySquare(firstRectangelSide, secondRectangelSide, square){
     return Math.floor(firstRectangelSide * secondRectangelSide / square);
 }
 
+//---------------------6---------------------
+let i = 2;
+let a = ++i;
+let b = i++;
 
-// //---------------------------------------------
+//---------------------7--------------------
+console.log('Привет' < 'привет');
+console.log('Привет' > 'Пока');
+console.log(45 < '53');
+console.log(false < 3);
+
+console.log(3 > '5мм');
+
+console.log(null < undefined);
+
+//---------------------8---------------------
 function checkPassphrase(){
     phrase = prompt('Введите секретную фразу', '');
     let passPhrase = 'ясекретнаяфраза';
-    if (String(phrase).toLowerCase() == passPhrase){
+    if (phrase.toLowerCase() == passPhrase){
         alert('Норм');
     }
     else{
         alert('не норм');
     }
 }
-// //---------------------------------------------
+//---------------------9---------------------
 
 function loginValidation(){
     let adminLogin = 'admin';
@@ -74,7 +90,7 @@ function loginValidation(){
         alert(message)
     }
 }
-// //----------------------------------------------
+//---------------------10---------------------
 function examResultsCheck(){
     let math = prompt('Оценка по математике: ');
     let english = prompt('Оценка по английскому: ');
@@ -104,13 +120,25 @@ function examResultsCheck(){
         alert('Вы перешли на следующий курс')
     }
 }
-//--------------------------------------------
+//---------------------11---------------------
 function getSum(){
     let a = prompt('Введите число а: ');
     let b = prompt('Введите число b: ');
     alert(Number(a) + Number(b));
 }
-//-------------------------------------------
+//---------------------12--------------------
+// console.log(true + true)
+// console.log(0 + "5")
+// console.log(5 + "мм")
+// console.log(8 / Infinity)
+// console.log(9 * "\n9")
+// console.log(null - 1)
+// console.log("5" - 2)
+// console.log("5px" - 3)
+// console.log(true - 3)
+// console.log(0 || 7)
+
+//---------------------13---------------------
 function neponaytnoChto(){
     for (let x = 1; x <= 10; x++){
         if (checkParity(x)){
@@ -127,13 +155,13 @@ function checkParity(number){
 
     return false;
 }
-// //--------------------------------------------
+//---------------------14---------------------
 function neponaytnoChto2(x){
-    while (x < 100){
+    while (x <= 100){
         x = prompt('Введите x')
     }
 }
-// //-------------------------------------------
+//---------------------15---------------------
 
 function getDay(){
     let number = prompt("Введите день недели")
@@ -162,20 +190,29 @@ function getDay(){
     }
 }
 
-// //-------------------------------------------
+//---------------------4---------------------
 console.log(getArea(45, 21));
+
+//---------------------5---------------------
 console.log(howManySquare(45, 21, 5));
 
+//---------------------8---------------------
 // checkPassphrase();
 
+//---------------------9---------------------
 // loginValidation();
 
-// examResultsCheck();
+//---------------------10--------------------
+examResultsCheck();
 
+//---------------------11--------------------
 // getSum();
 
+//---------------------13--------------------
 // neponaytnoChto();
 
+//---------------------14--------------------
 // neponaytnoChto2(`${prompt('Введите x')}`)
 
-getDay()
+//---------------------14--------------------
+// getDay()
--- Course 2/Term 1/User Interface Development Technologies/02. Javascript Basics/main.js
+++ Course 2/Term 1/User Interface Development Technologies/02. Javascript Basics/main.js
@@ -1,48 +1,160 @@
-// function getSquareDiameterLengthCircle(){
-//     const pi = 3.1415926;
-//     let radius = prompt('Введите радиус окружности');
+//---------------------1---------------------
+function getSquareDiameterLengthCircle(){
+    const pi = 3.1415926;
+    let radius = prompt('Введите радиус окружности');
 
-//     alert(`Длина окружности - ${2*pi*radius} \nПлощадь окружности - ${pi*radius**2} \nДиаметр окружности - ${radius*2}`);
-// }
-
-// getSquareDiameterLengthCircle()
-
-// function userMessage(){
-//     let sum = 0;
-//     let userMoney = 3;
-//     let productPrice = 1;
-
-//     while(productPrice > 0){
-//         productPrice = prompt('Введите цену товара')
-//         if ((sum + Number(productPrice)) > userMoney){
-//             alert('Недостаточно средств на балансе\nВыберите другой товар или завершите покупку')
-//         }
-//         else{
-//             sum += Number(productPrice)
-//         }
-//     }
-
-//     return sum
-// }
-
-// alert(userMessage())
-
-// function concatStrings(firstString, lastString, space = ' '){
-//     return firstString + space + lastString
-// }
-
-// let firstString = prompt('Введите первую строку:')
-// let lastString = prompt('Введите последнию строку:')
-
-// alert(concatStrings(firstString, lastString))
-
-function counterOfStudents(){
-    let student = 1;
-    while (student){
-        let counter = 0;
-        student = prompt(`${counter++} Имя и фамилия`)
-    }
-    return counter;
+    alert(`Длина окружности - ${2*pi*radius} \nПлощадь окружности - ${pi*radius**2} \nДиаметр окружности - ${radius*2}`);
 }
 
-counterOfStudents()
+let getSquareDiameterLengthCircleExpression = function() {
+    const pi = 3.1415926;
+    let radius = prompt('Введите радиус окружности');
+
+    alert(`Длина окружности - ${2*pi*radius} \nПлощадь окружности - ${pi*radius**2} \nДиаметр окружности - ${radius*2}`);
+}
+
+let getSquareDiameterLengthCircleArrow = () =>{
+    const pi = 3.1415926;
+    let radius = prompt('Введите радиус окружности');
+    alert(`Длина окружности - ${2*pi*radius} \nПлощадь окружности - ${pi*radius**2} \nДиаметр окружности - ${radius*2}`);
+}
+
+//---------------------2---------------------
+// let firstString = prompt('Введите первую строку:')
+// let lastString = prompt('Введите вторую строку:')
+
+// let concatStrings = (firstString, secondString, space = ' ') => firstString + space + secondString
+
+//---------------------3---------------------
+function counterOfStudents(){
+    
+    let listOfStudents = [];
+    let student = 1;
+    let counter = 1;
+
+    while (student){
+        student = prompt(`${printListOfStudent(listOfStudents)}`)
+        counter++;
+        listOfStudents.push(student)
+    }
+
+    alert(`${text}Всего студентов - ${counter - 2}`)
+}
+
+function printListOfStudent(listOfStudents) {
+    text = '';
+    for (let x = 0 ; x < listOfStudents.length; x++){
+        text += (`${x + 1}. ${listOfStudents[x]} \n`)
+    }
+    return text;
+}
+
+//---------------------4---------------------
+function howMuchTime(){
+    let amount = 26 * 26 * 26 * 26 * 26 * 10 * 10 * 10;
+
+    const NumOfSecInY = 365 * 24 * 60 * 60;
+    const NumOfSecInM = 30 * 24 * 60 * 60;
+    const NumOfSecInD = 24 * 60 * 60;
+    const NumOfSecInH = 60 * 60;
+    const NumOfSecInMin = 60;
+
+    let seconds = amount * 3;
+
+    let years = Math.floor(seconds / NumOfSecInY);
+
+    let months = Math.floor((seconds - years * NumOfSecInY) / NumOfSecInM);
+
+    let days = Math.floor((seconds - years * NumOfSecInY - months * NumOfSecInM) 
+                          / NumOfSecInD);
+
+    let hours = Math.floor((seconds - years * NumOfSecInY - months * NumOfSecInM
+        - days * NumOfSecInD) / NumOfSecInH);
+
+    let minutes = Math.floor((seconds - years * NumOfSecInY - months * NumOfSecInM
+        - days * NumOfSecInD - hours * NumOfSecInH)/ NumOfSecInMin);
+
+    seconds = seconds - years * NumOfSecInY - months * NumOfSecInM
+        - days * NumOfSecInD - hours * NumOfSecInH - minutes * NumOfSecInMin;
+
+    alert(`${years} лет ${months} месяцев ${days} дней ${hours} часов ${minutes} минут ${seconds} секунд`)
+}
+
+//---------------------5---------------------
+function func(){
+    let data = prompt('Введите данные');
+
+    if (Number(data)){
+        alert(`Округление в меньшую сторону - ${parseFloat(Math.floor(data)).toString(16).toUpperCase()}\nОкругление в большую сторону - ${parseFloat(Math.ceil(data)).toString(16).toUpperCase()}\nОкругление до ближайшего целого - ${parseFloat(Math.round(data)).toString(16).toUpperCase()}\n`)
+    }
+    else{
+        alert(`${data.toUpperCase()} \n${data.toLowerCase()}`)
+    }
+
+}
+
+//---------------------6---------------------
+function getMistakeIfExist(firstString, secondString) {
+    for(let i = 0; i < firstString.length; i++){
+        if (firstString[i] != secondString[i]){
+            return (i + 1)
+        }
+    }
+    return 0;
+}
+
+function checkCT(){
+    const word = 'алгоритм';
+    let userTry = prompt('Введите ваш вариант').toLowerCase();
+
+    let Check = (mistakeIfExist = getMistakeIfExist(word, userTry)) ?
+    () => alert(`Ошибка в позиции ${mistakeIfExist}`) :
+    () => alert('Правильно');
+
+    Check();
+}
+
+//---------------------7---------------------
+function geometryCalculator(a, b){
+    let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+
+    let sina = b/c
+    let cosa = a/c
+    let tga = b/a
+    let ctga = a/b
+
+    let sinb = a/c
+    let cosb = b/c
+    let tgb = a/b
+    let ctgb = b/a
+
+    let square = 0.5 * a * b
+    let height = (2 * square) / c
+    let perimeter = a + b + c
+
+
+    alert(`sin(a) - ${sina} \ncos(a) - ${cosa} \ntg(a) - ${tga} \nctg(a) - ${ctga} \n\nsin(b) - ${sinb} \ncos(b) - ${cosb} \ntg(b) - ${tgb} \nctg(b) - ${ctgb}  \n\nПериметр - ${perimeter} \nПлощадь - ${square} \nВысота - ${height}`)
+}
+
+//---------------------1---------------------
+// getSquareDiameterLengthCircle()
+// getSquareDiameterLengthCircleExpression()
+// getSquareDiameterLengthCircleArrow()
+
+//---------------------2---------------------
+// alert(concatStrings(firstString, lastString))
+
+//---------------------3---------------------
+// counterOfStudents()
+
+//---------------------4---------------------
+// howMuchTime();
+
+//---------------------5---------------------
+func()
+
+//---------------------6---------------------
+// checkCT()
+
+//---------------------7---------------------
+// geometryCalculator(4, 3)
