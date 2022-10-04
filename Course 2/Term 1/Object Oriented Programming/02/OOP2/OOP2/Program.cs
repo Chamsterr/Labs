f0d2862cfb9e141f2560cd75048799857970f721
Chamster <nikkovkel258@gmail.com>
2022-10-04 13:36:24 +0300

    OOP (ООП) "7-10"
    Signed-off-by: Chamster <nikkovkel258@gmail.com>

--- Course 2/Term 1/Object Oriented Programming/02/OOP2/OOP2/Program.cs
+++ Course 2/Term 1/Object Oriented Programming/02/OOP2/OOP2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,16 +23,77 @@ class Test
 }
 class House
 {
+    int apartmentNumberBase = 1;
+    int floorBase = 1;
+    int squareBase = 1;
+    int numberOfRoomsBase = 1;
     static int counter = 0;
+    static int currentYear;
+
 
     public readonly int id;
-    public int apartmentNumber {get;set;}
+    public int apartmentNumber 
+    {
+        set
+        {
+            if (apartmentNumber < 0)
+                Console.WriteLine("Номер квартиры должен быть положительный");
+            else
+                apartmentNumberBase = value;
+        }
 
-    public int square { get; set; }
-    public int floor { get; set; }
-    public int numberOfRooms { get; set; }
+        get
+        {
+            return apartmentNumberBase;
+        }
+    }
+    private int square 
+    {
+        set
+        {
+            if (value <= 0)
+                Console.WriteLine("Площадь должен быть положительной");
+            else
+                squareBase = value;
+        }
+
+        get
+        {
+            return squareBase;
+        }
+    }
+    public int floor  
+    {   
+        set
+        {
+            if (value <= 1 || value >= 300)
+                Console.WriteLine("Этаж должен быть в диапозоне от 1 до 300");
+            else
+                floorBase = value;
+        }
+
+        get
+        {
+            return floorBase;
+        }
+    }
+    public int numberOfRooms 
+    {
+         set
+        {
+            if (numberOfRoomsBase > 1)
+                Console.WriteLine("Количество комнат должно быть положительным: ");
+            else
+                numberOfRoomsBase = value;
+        }
+
+        get
+        {
+            return numberOfRoomsBase;
+        }
+    }
     public string adress { get; set; }
-    public string buildingType { get; set; }
+    private string buildingType { get; set; }
     public int lifetime { get; set; }
     
 
@@ -48,19 +110,17 @@ class House
         this.lifetime = lifetime;
         id = (apartmentNumber * adress.Length) / numberOfRooms * square / floor;
     }
-
     public House() {
         counter++;
         apartmentNumber = 1;
         square = 135;
-        floor = 1;
+        floor = 3;
         numberOfRooms = 1;
         adress = "Peresedenskay";
         buildingType = "mansion";
         lifetime = 2024;
         id = GetHashCode();
     }
-
     public House(int apartmentNumber, int square, int floor, int numberOfRooms, string adress, string buildingType)
     {
         counter++;
@@ -72,14 +132,16 @@ class House
         this.buildingType = buildingType;
         id = GetHashCode();
     }
+    static House()
+    {
+        currentYear = DateTime.Now.Year; 
+    }
 
 
     public void Overhaul(ref int lifetime, out int whenOverhaul)
     {
-        whenOverhaul = lifetime - DateTime.Now.Year;
+        whenOverhaul = lifetime - currentYear;
     }
-
-
     static public void PrintInfoAboutClass()
     {
         Console.WriteLine($"Это класс House\n");
@@ -96,6 +158,7 @@ class House
                           $"Тип здания - {buildingType}  \n" +
                           $"Cрок эксплуатации до {lifetime}");
     }
+
 
     public override int GetHashCode()
     {
@@ -138,25 +201,82 @@ namespace OOP2
             Console.WriteLine(house.Equals(house2));
             Console.WriteLine(house.GetType());
 
-            House[] listOfHouses = new House[] { house, house2 };
-            House[] listOfHousesThatHaveСertainApart = new House[10];
+            House[]? listOfHouses = new House[] { house, house2 };
+            List<House> listOfHousesThatHaveСertainRooms = new List<House>();
+
+            List<House> listOfHousesThatHaveСertainRoomsInDist = new List<House>();
 
             listOfHouses[0] = house;
             listOfHouses[1] = house2;
 
-            Console.Write("Введите количество квартир ");
+            Console.WriteLine();
+            Console.Write("Введите количество комнат: ");
             int? amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите нижнюю границу между этажами: ");
+            int? distBetweenFloor1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Введите верхнию границу между этажами: ");
+            int? distBetweenFloor2 = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < listOfHouses.Length; i++)
             {
                 if (listOfHouses[i].numberOfRooms == amount)
                 {
-                    listOfHousesThatHaveСertainApart.Append(listOfHouses[i]);
-                    Console.WriteLine(listOfHouses[i].adress + " " + listOfHouses[i].apartmentNumber);
+                    listOfHousesThatHaveСertainRooms.Add(listOfHouses[i]);
+                }
+
+                if (listOfHouses[i].numberOfRooms == amount && distBetweenFloor1 <= listOfHouses[i].floor && listOfHouses[i].floor <= distBetweenFloor2)
+                {
+                    listOfHousesThatHaveСertainRoomsInDist.Add(listOfHouses[i]);
                 }
             }
-            
 
-        }
+            if (listOfHousesThatHaveСertainRooms.Count == 0)
+            {
+                Console.WriteLine("Таких квартир нет: ");
+            }
+            else
+            {
+                Console.WriteLine("Комнаты которые имеют заданное число комнат:");
+            }
+
+            for (int i = 0; i < listOfHousesThatHaveСertainRooms.Count; i++)
+            {
+                if (listOfHousesThatHaveСertainRooms[i] != null) 
+                {
+                    Console.WriteLine(listOfHousesThatHaveСertainRooms[i].adress + " " + listOfHousesThatHaveСertainRooms[i].apartmentNumber);
+                }
+            }
+
+            if (listOfHousesThatHaveСertainRoomsInDist.Count == 0)
+            {
+                Console.WriteLine("Таких квартир нет ");
+            }
+            else
+            {
+                Console.WriteLine("Комнаты которые имеют заданное число комнат:");
+            }
+
+            for (int i = 0; i < listOfHousesThatHaveСertainRoomsInDist.Count; i++)
+            {
+                if (listOfHousesThatHaveСertainRooms[i] != null)
+                {
+                    Console.WriteLine(listOfHousesThatHaveСertainRoomsInDist[i].adress + " " + listOfHousesThatHaveСertainRoomsInDist[i].apartmentNumber);
+                }
+            }
+
+            var house3 = new {id= 13, apartmentNumber = 24, square = 34, floor = 4, numberOfRooms = 3, adress = "Gastello", buildingType = "flat", lifetime = 2027 };
+
+            Console.WriteLine($" \nId - {house3.id} \n" +
+                              $"Номер квартиры - {house3.apartmentNumber} \n" +
+                              $"Площадь - {house3.square} \n" +
+                              $"Этаж - {house3.floor}  \n" +
+                              $"Количество комнат - {house3.numberOfRooms}  \n" +
+                              $"Улица - {house3.adress}  \n" +
+                              $"Тип здания - {house3.buildingType}  \n" +
+                              $"Cрок эксплуатации до {house3.lifetime}");
+            }
     }
 }
---
+++ Course 2/Term 1/Object Oriented Programming/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class House
+{
+    public int id;
+    public int apartmentNumber;
+    public int square;
+    public int floor;
+    public int numberOfRooms;
+    public string street;
+    public string buildingType;
+    public DateTime lifetime;
+}
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
