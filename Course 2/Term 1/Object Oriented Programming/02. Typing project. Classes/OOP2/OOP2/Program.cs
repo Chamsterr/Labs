2b9f53084a5580afd24bf062bda9d8dfbba561e5
Chamster <nikkovkel258@gmail.com>
2022-10-05 23:57:56 +0300

    OOP (ООП)

---
+++ Course 2/Term 1/Object Oriented Programming/02. Typing project. Classes/OOP2/OOP2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+class Program
+{
+    struct Point
+    {
+        public int x, y;
+        public override string ToString()
+        {
+            return x.ToString() + y.ToString();
+        }
+    }
+    static void Main(string[] args)
+    {
+        Point obj = new Point(); //1
+        System.Console.WriteLine(obj); //2
+    }
+}
