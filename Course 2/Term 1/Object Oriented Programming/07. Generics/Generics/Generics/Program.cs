051483ab7214a1499990e9ab63f1db8ed417063a
Chamster <nikkovkel258@gmail.com>
2022-11-29 11:39:57 +0300

    OOP (ООП) "Add write, read methods"

---
+++ Course 2/Term 1/Object Oriented Programming/07. Generics/Generics/Generics/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+interface I<T>
+{
+    void Add(T item);
+    void Remove(T item);
+
+    List<T> FindGreater(T x);
+    List<T> FindLess(T x);
+}
+
+
+public class Set<T> : I<T>
+{
+    public List<T> numbers = new List<T>();
+
+    public Set()
+    {
+        numbers = new List<T>();
+    }
+    public void Add(T item)
+    {
+        numbers.Add(item);
+    }
+    public void Remove(T item)
+    {
+        numbers.Remove(item);
+    }
+    public List<T>? FindGreater(T x)
+    {
+        List<T>? answers = new List<T>() ;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int? o;
+            int? oo;
+
+            if (numbers[i] is int)
+            {
+                o = numbers[i] as int?;
+                oo = x as int?;
+            }
+            else if (numbers[i] is char)
+            {
+                o = numbers[i] as char?;
+                oo = x as char?;
+            }
+            else
+            {
+                throw new WrongType("FindGreater работает только для типвов char и int");
+                return answers;
+            }
+
+            if (o > oo)
+            {
+                answers.Add(numbers[i]);
+            }
+         }
+            return answers;
+    }
+
+    public List<T>? FindLess(T x)
+    {
+        List<T>? answers = new List<T>();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int? o;
+            int? oo;
+
+            if (numbers[i] is int)
+            {
+                o = numbers[i] as int?;
+                oo = x as int?;
+            }
+            else if (numbers[i] is char)
+            {
+                o = numbers[i] as char?;
+                oo = x as char?;
+            }
+            else
+            {
+                throw new WrongType("FindLess работает только для типвов char и int");
+                return answers;
+            }
+
+            if (o < oo)
+            {
+                answers.Add(numbers[i]);
+            }
+        }
+        return answers;
+    }
+    public Set(List<T> numbers)
+    {
+        this.numbers = numbers;
+    }
+
+    public static Set<T> operator +(Set<T> set, Set<T> set2)
+    {
+        foreach (T i in set2.numbers)
+        {
+            set.numbers.Add(i);
+        }
+        return set;
+    }
+    public static bool operator >=(Set<T> set, Set<T> set2)
+    {
+        if (set.numbers.Count >= set2.numbers.Count)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static bool operator <=(Set<T> set, Set<T> set2)
+    {
+        if (set.numbers.Count <= set2.numbers.Count)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static implicit operator int(Set<T> set)
+    {
+        return set.numbers.Count;
+    }
+    public static T operator %(Set<T> set, int i)
+    {
+        return set.numbers[i];
+    }
+    public void PrintSet()
+    {
+        foreach (T i in numbers)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+    }
+
+    public List<T>? Check(T x)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Write(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename, false))
+        {
+            writer.WriteLineAsync($"Set - {this.ToString()}, кол-во: {this.numbers.Count}");
+        }
+    }
+
+    public void Read(string filename)
+    {
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string text = reader.ReadToEnd();
+            Console.WriteLine(text);
+        }
+    }
+}
+
+public class QuestionsList<T> where T: Question 
+{
+    public List<T> list = new List<T>();
+    public QuestionsList()
+    {
+        list = new List<T>();
+    }
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Set<int> set = new Set<int>();
+        set.Add(1);
+        set.Add(2);
+        set.Add(3);
+
+        foreach (int i in set.FindGreater(1))
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("---------------------------------");
+        foreach (int i in set.FindLess(4))
+        {
+            Console.WriteLine(i);
+        }
+
+        Set<char> set_str = new Set<char>();
+        set_str.Add('a');
+        set_str.Add('b');
+        set_str.Add('c');
+
+        Console.WriteLine("---------------------------------");
+        foreach (char i in set_str.FindGreater('a'))
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("---------------------------------");
+        foreach (char i in set_str.FindLess('c'))
+        {
+            Console.WriteLine(i);
+        }
+        set.Write("log.txt");
+        set.Read("log.txt");
+    }
+}
---
+++ Course 2/Term 1/Object Oriented Programming/07. Generics/Generics/Generics/Question.cs
@@ -0,0 +1,45 @@
+﻿public abstract class Challenge
+{
+    public bool passOrNo;
+    public abstract bool tryToPass();
+    public abstract object DoClone();
+
+}
+
+public class Question : Challenge
+{
+    public string? question { get; set; }
+    public string? questionAnswer { get; set; }
+    public string? guess { get; set; }
+
+    public Question()
+    {
+        question = questionAnswer = guess = null;
+    }
+    public Question(string question, string questionAnswer)
+    {
+        this.question = question;
+        this.questionAnswer = questionAnswer;
+    }
+
+    public override bool tryToPass()
+    {
+        if (questionAnswer == guess)
+        {
+            passOrNo = true;
+            return passOrNo;
+        }
+        passOrNo = false;
+        return passOrNo;
+    }
+
+    public override object DoClone()
+    {
+        return new Question(question, questionAnswer);
+    }
+
+    public override string ToString()
+    {
+        return question;
+    }
+}
